a11cee7d4b4f7d9ca1c42cb7f0affd507fdf8b9a
Praveen Rajak <praveenrajak0506@gmail.com>
2025-08-21 23:59:57 +0530

    Landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 'use client'
-// import LandingPage from './components/LandingPage';
-import ComingSoon from './components/ComingSoon';
+import LandingPage from './components/LandingPage';
+// import ComingSoon from './components/ComingSoon';
 
 export default function Home(){
   return(
     <>
-      <ComingSoon />
+      <LandingPage />
     </>
   )
 }
