b5ea5a9ec6de4afe23b0377e81208b5adad16c62
Praveen Rajak <praveenrajak0506@gmail.com>
2025-10-01 01:00:07 +0530

    Working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,7 @@ import LandingPage from './components/LandingPage';
 export default function Home(){
   return(
     <>
-      {/* <LandingPage /> */}
-
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
-      <p>
-        This site is temporarily off. Please check back soon!
-      </p>
-    </div>
+      <LandingPage />
     </>
   )
 }
